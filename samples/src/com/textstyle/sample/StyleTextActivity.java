c5b03d02dde8241693541d968579918efa3cdb04
Raymond Chenon <raymond.chenon@gmail.com>
2013-07-17 01:03:47 +0200

    add the style example.

---
+++ samples/src/com/textstyle/sample/StyleTextActivity.java
@@ -0,0 +1,22 @@
+package com.textstyle.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: raychenon
+ * Date: 17/7/13
+ * Time: 12:34 AM
+ */
+public class StyleTextActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avt_style);
+
+    }
+
+}
